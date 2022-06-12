5ec8548f46af75e8714befed0962085e1d805d8a
Manoel Lima <manoel.lima@tjac.jus.br>
2022-06-12 13:36:13 -0400

    Controller for negociation created

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,28 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+
+        console.log(negociacao);
+    }
+
+    private criaNegociacao(): Negociacao {
+        const exp = /-/g; // localiza todas as ocorrências de -
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(date, quantidade, valor);
+        return negociacao;
+    }
+}
