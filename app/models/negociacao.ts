9dfb26ad34f2136f9692925c91b573bdbeba0422
Manoel Lima <manoel.lima@tjac.jus.br>
2022-06-12 01:45:37 -0400

    Ajuste no código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -4,9 +4,9 @@ export class Negociacao {
     #valor;
 
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
     }
 
     get data() {
